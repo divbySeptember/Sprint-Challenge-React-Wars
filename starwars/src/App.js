5e40a4bc3ce605e9141245e32ab6ccd33353542d
Anthony Johnson <lambda_school_loaner_82@Lambda-School-Loaner-82s-MacBook-Pro.local>
2019-03-22 12:35:52 -0400

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CharacterList from './components/CharacterList';
 import './App.css';
 
 class App extends Component {
@@ -10,18 +11,21 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters('https://swapi.co/api/people');
   }
 
   getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
+    console.log(URL);
+    
     fetch(URL)
       .then(res => {
         return res.json();
       })
       .then(data => {
+        console.log(data);
         this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
@@ -33,6 +37,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharacterList characters={this.state.starwarsChars}/>
       </div>
     );
   }
